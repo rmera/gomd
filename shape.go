64c1a42e291442d80a36f6e7ca10b9b800566a70
rmera <>
2022-04-06 01:10:31 -0400

    Implemented analysis for the angle between SOD1 monomers

--- shape.go
+++ shape.go
@@ -1,9 +1,113 @@
 package main
 
 import (
+	"log"
+
 	chem "github.com/rmera/gochem"
 	v3 "github.com/rmera/gochem/v3"
 )
+
+//EasyShape takes a matrix of coordinates, a value for epsilon (a number close to zero, the closer, the more
+//strict the orthogonality requriements are) and an (optative) masser and returns
+//two shape indicators based on the elipsoid of inertia (or it massless equivalent)
+//a linear and circular distortion indicators, and an error or nil (in that order).
+//if you give a negative number as epsilon, the default (quite strict) will be used.
+func FirstEig(coords *v3.Matrix, epsilon float64, mol ...chem.Masser) (*v3.Matrix, error) {
+	var masses []float64
+	var err error
+	if len(mol) < 0 {
+		masses = nil
+	} else {
+		masses, err = mol[0].Masses()
+		if err != nil {
+			masses = nil
+			log.Println("FirstEig: Masses not found, will set them all to 1")
+		}
+	}
+	moment, err := chem.MomentTensor(coords, masses)
+	if err != nil {
+		return nil, err
+	}
+	evecs, evals, err := v3.EigenWrap(moment, epsilon)
+	if err != nil {
+		return nil, err
+	}
+	log.Println(evals)
+
+	return evecs.VecView(2), nil //The last and largest evec (I think!)
+
+}
+
+//Dimersangle is a specific function for SOD1 analysis. It will not go into the main
+func DimersAngle(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
+	//	fmt.Println("Use: MDan RMSD sel1 sel2...")
+	argslen := len(args)
+	epsilon := 0.0001
+	if argslen < 4 {
+		panic("DimersAngle: Not enough arguments, need 4!")
+	}
+
+	indexes := make([][]int, 0, argslen)
+	for _, v := range args {
+		s, err := sel2atoms(mol, v)
+		if err != nil {
+			panic("Shape: sel2atoms:" + err.Error())
+		}
+		indexes = append(indexes, s)
+	}
+
+	refmol := chem.NewTopology(0, 1)
+	refs := make([]*v3.Matrix, 0, len(indexes))
+
+	for _, v := range indexes {
+		tr := v3.Zeros(len(v))
+		refs = append(refs, tr)
+	}
+	//temporal vectors
+	tp := v3.Zeros(1)
+	tp2 := v3.Zeros(1)
+	p := v3.Zeros(1)
+
+	ret := func(coord *v3.Matrix) []float64 {
+		vectors := make([]*v3.Matrix, 0, 2)
+		planes := make([]*v3.Matrix, 0, 2)
+		for i, v := range indexes {
+			refmol.SomeAtoms(mol, v)
+			refs[i].SomeVecs(coord, v)
+			if i < 2 {
+				tv, err := FirstEig(refs[i], epsilon, refmol)
+				if err != nil {
+					panic(err.Error)
+				}
+				vectors = append(vectors, tv)
+
+			} else {
+				tv, err := chem.BestPlane(refs[i], refmol)
+				if err != nil {
+					panic(err.Error)
+				}
+				planes = append(planes, tv)
+			}
+		}
+		//We have the data, now comes the tricky part!
+		ang := chem.Angle(planes[0], planes[1])
+		ang *= chem.Rad2Deg
+		if ang > 90 {
+			planes[1].Scale(-1, planes[1]) //Should ensure that vectors are not "inverted"
+		}
+		tp.Add(planes[0], planes[1])
+		p.Scale(0.5, tp)
+		tp.Cross(p, vectors[0])
+		tp2.Cross(p, tp)
+		prv1 := chem.Projection(vectors[0], tp2)
+		tp.Cross(p, vectors[1])
+		tp2.Cross(p, tp)
+		prv2 := chem.Projection(vectors[1], tp2)
+		return []float64{chem.Angle(prv1, prv2) * chem.Rad2Deg}
+	}
+	return ret
+
+}
 
 //PlaneAngle function that will calculate the angle between the best planes for two selections, and do that for as many pairs of selections as requested.
 func PlanesAngle(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
