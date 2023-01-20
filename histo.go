1934e74806746f62aa5943c4720e8e153b9c223c
rmera <>
2023-01-19 21:44:19 -0300

    Started adding histogram funcionality
    The plan is to just build the histograms
    and put them  in a JSON file. We can add a second
    Go program to combine different histograms
    and a Python script to plot the result.

---
+++ histo.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"log"
+
+	chem "github.com/rmera/gochem"
+	"github.com/rmera/gochem/histo"
+	v3 "github.com/rmera/gochem/v3"
+	"github.com/rmera/scu"
+)
+
+//InterByRes returns a list with the residues involved in an interface, on each frame. As goMD functions needs to return floats,
+//the list of residues will be in that format, but they will be integer numbers.
+//this requires the "voro" package in goChem, which is not yet in master or devel.
+//If both of the selections are from different chains, the residues from the second selection will be given as
+//negative numbers, to distinguish them from residues from the other chain and the same residue ID..
+func DistanceHistogramMatrix(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
+	//	fmt.Println("Use: MDan RMSD sel1 sel2...")
+	argslen := len(args)
+	name := "CA"
+	if argslen < 1 {
+		log.Println("Name for the backbone atom not given, will use 'CA'")
+	} else {
+		name = args[0]
+	}
+	step := 0.1
+	if argslen > 1 {
+		step = scu.MustAtoI(args[1])
+	}
+	first := 0
+	last := 16
+	if argslen > 2 {
+		last = scu.MustAtoI(args[2]) //I'm lazy
+	}
+
+	//I need the number of residues
+	Nres := 1
+	currres := mol.Atom(0).MolID
+	for i := 0; i < mol.Len(); i++ {
+		at := mol.Atom(i)
+		if at.MolID != currres {
+			Nres++
+			currres = at.MolID
+		}
+	}
+	//now the dividers
+	divs = make([]float64, 0, step*last)
+	for i := first; i <= last; i += step {
+		divs = append(divs, i)
+	}
+	matrix := histo.NewMatrix(Nres, Nres, divs)
+	for i := 0; i < Nres; i++ {
+		for j := 0; j < Nres; j++ {
+			matrix.NewHisto(i, j)
+		}
+	}
+	backboneindex := make([]float64, 0, Nres)
+	for i := 0; i < mol.Len(); i++ {
+		at1 = mol.Atom(i)
+		if at1.Name != name {
+			continue
+		}
+		backboneindex = append(backboneindex, i)
+	}
+
+	var at1, at2 *chem.Atom
+	distvec := v3.Zeros(1)
+	ret := func(coord *v3.Matrix) []float64 {
+		for i, v := range backboneindex[:len(backboneindex)-1] {
+			for j, w := range backboneindex[i+1:] {
+				v1 := coord.VecView(v)
+				v2 := coords.VecView(w)
+				distvec.Sub(v2, v1)
+				matrix.AddData(v, w, distvec.Norm(2))
+			}
+
+		}
+		return []float64{0}
+	}
+	return ret, matrix //the caller will have to deal with the final histogram.
+	//It should be just one call to put it in a JSON file.
+}
