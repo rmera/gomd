1d95213d7b7b1752ea902ec9c98ce08fef6f22d7
rmera <>
2023-02-28 22:41:21 -0300

    Added feature to planesangle
    Now you can obtain the angle between the best plane passing throught
    a set of atoms, and the plane for the same set of atoms in the reference
    strucure.

--- shape.go
+++ shape.go
@@ -5,9 +5,81 @@ import (
 	v3 "github.com/rmera/gochem/v3"
 )
 
-//PlaneAngle function that will calculate the angle between the best planes for two selections, and do that for as many pairs of selections as requested.
-func PlanesAngle(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
+//PlaneAngle return function that will calculate the angle between the best plane for a selection in a test
+// and a reference structure. The reference and test structures are assumed to be superimposed.
+func PlaneAngle(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
 	//	fmt.Println("Use: MDan RMSD sel1 sel2...")
+	argslen := len(args)
+	if argslen < 1 {
+		panic("PlanesAngle: Need at least one argument!")
+	}
+	indexes := make([][]int, 0, argslen)
+	for _, v := range args {
+		s, err := sel2atoms(mol, v)
+		if err != nil {
+			panic("PlanesAngle: sel2atoms:" + err.Error())
+		}
+		indexes = append(indexes, s)
+	}
+	refmol1 := chem.NewTopology(0, 1)
+	tests := make([]*v3.Matrix, 0, len(indexes)) //keeps temporary coordinates for plane calculations
+	refs := make([]*v3.Matrix, 0, len(indexes))  //keeps the reference plane vectors
+	for _, v := range indexes {
+		tt := v3.Zeros(len(v))
+		tests = append(tests, tt)
+	}
+	//we first obtain the vectors normal to the planes in the ref structure
+	for i, v := range indexes {
+		refmol1.SomeAtoms(mol, v)
+		tests[i].SomeVecs(mol.Coords[0], v)
+		p1, err := chem.BestPlane(tests[i], refmol1)
+		if err != nil {
+			panic("PlanesAngle: " + err.Error())
+		}
+		refs = append(refs, p1)
+
+	}
+	ret := func(coord *v3.Matrix) []float64 {
+		pangles := make([]float64, 0, len(indexes))
+		for i, v := range indexes {
+			//for i, v := range indexes {
+			refmol1.SomeAtoms(mol, v)
+			tests[i].SomeVecs(coord, v)
+			p2, err := chem.BestPlane(tests[i], refmol1)
+			if err != nil {
+				panic("PlanesAngle: " + err.Error())
+			}
+			ang := chem.Angle(refs[i], p2)
+			ang *= chem.Rad2Deg
+			if ang > 90 {
+				ang = 180 - ang //We can't make this distinction, because the orientation of the vector normal to the moment-of-inertia plane can vary with small changes in the position of the atoms involved.
+			}
+			pangles = append(pangles, ang)
+
+		}
+		return pangles
+	}
+	return ret
+}
+
+//returns a function that will either obtain the angle between the planes for pairs of selections (if the "comp" option
+//is given as the first option) or between the plane for a selection and the plane for the same selection in the reference
+//structure (if the "comp" option is not given).
+func PlanesAngle(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
+	argslen := len(args)
+	if argslen < 2 {
+		panic("PlanesAngle: Need at least 1 arguments (3 if the 'comp' options is given)!")
+	}
+	if args[0] == "pairs" {
+		return CompPlanesAngle(mol, args[1:])
+
+	}
+	return PlaneAngle(mol, args)
+
+}
+
+//CompPlanesAngle function that will calculate the angle between the best planes for two selections, and do that for as many pairs of selections as requested.
+func CompPlanesAngle(mol *chem.Molecule, args []string) func(coord *v3.Matrix) []float64 {
 	argslen := len(args)
 	if argslen%2 != 0 || argslen < 2 {
 		panic("PlanesAngle: Need a greater than 0 even number of arguments!")
